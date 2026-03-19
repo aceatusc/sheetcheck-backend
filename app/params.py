06047c8f3be805f4b26a67baa168ab4e2082cebe
Sadra Sabouri <sabouri.sadra@gmail.com>
2026-03-18 19:22:18 -0700

    Revert "change : number of loaded requirements decreased."
    This reverts commit 4fe463af0ef40eb5021d981c307db55466e6b9ff.

--- app/params.py
+++ app/params.py
@@ -291,7 +291,7 @@ The user will describe their task. Respond with ONLY a JSON object:
   ]
 }
 
-Generate 1-2 hard and 1-2 soft requirements. Hard = must-have correctness criteria. Soft = nice-to have criteria.
+Generate 2-3 hard and 3-4 soft requirements. Hard = must-have correctness criteria. Soft = quality/style preferences.
 Respond with ONLY the JSON, no markdown.
 """,
 
